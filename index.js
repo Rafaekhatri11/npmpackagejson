f5e7f722faab89c5ca16b9ccf9009c4eb921e8a3
Rafaekhatri11 <Rafaekhatri11@gmail.com>
2020-11-12 13:07:24 +0500

    _api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-
+var path = require("path")
 app.get('/', function(req, res){
-  res.send('Hello World');
+  // res.send('Hello World');
+  res.sendFile(path.join(__dirname,"./_api/package.json"))
 });
 const port =  process.env.PORT || 4000;
 var server = app.listen(port, function() {
