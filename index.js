3b376426a81382a468113c7d05f9d786b6caf907
Rafaekhatri11 <Rafaekhatri11@gmail.com>
2020-11-12 12:17:03 +0500

    some change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = express(); // here I use the express() method, instead of the createServer()
+var app = express();
 
 app.get('/', function(req, res){
   res.send('Hello World');
