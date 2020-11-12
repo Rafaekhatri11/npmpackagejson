a3ae1e85e46af0c81843aaab0a4e2ac805a29f64
Rafaekhatri11 <Rafaekhatri11@gmail.com>
2020-11-12 11:44:15 +0500

    port condition change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express(); // here I use the express() method, instead of the createSe
 app.get('/', function(req, res){
   res.send('Hello World');
 });
-const port = 4000 || process.env.PORT;
+const port =  process.env.PORT || 4000;
 var server = app.listen(port, function() {
   console.log('Listening on port %d', server.address().port);
 });
